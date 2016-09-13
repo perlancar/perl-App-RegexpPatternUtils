ae7161b946ab04e9f895c0cdb3bfacc86d01de1e
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-09-13 13:03:40 +0700

    Initial commit

---
+++ lib/App/RegexpPatternUtils.pm
@@ -0,0 +1,19 @@
+package App::RegexpPatternUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT:
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities related to L<Regexp::Pattern>:
+
+#INSERT_EXECS_LIST
